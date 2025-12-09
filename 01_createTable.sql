60c31a09d0054abdbd989fcc421fa2e54fc5f869
bcpeeres-ui <bcpeeres@gmail.com>
2025-12-08 23:11:59 -0400

    Criando script das tabelas do minimundo 01_createTable.sql

--- 01_createTable.sql
+++ 01_createTable.sql
@@ -1,21 +1,44 @@
-select * from Servico;
+BEGIN TRANSACTION;
 
-select DISTINCT nome
-from Servico;
+-------TABELA CLIENTE---------
+Create table Cliente (
+    id_cliente INTEGER PRIMARY KEY AUTOINCREMENT,
+    nome TEXT(100) NOT NULL,
+    telefone TEXT(15) UNIQUE,
+    email TEXT(50) UNIQUE,
+    data_cadastro DATETIME
+);
 
-Delete from Servico
-where id_servico = 1;
+--------TABELA PROFISSIONAL--------
+Create table Profissional (
+    id_profissional INTEGER PRIMARY KEY AUTOINCREMENT,
+    nome TEXT(100) NOT NULL,
+    cargo TEXT(30) NOT NULL,
+    comissao DECIMAL NOT NULL
+);
 
-Delete from Profissional
-where id_profissional = 2;
+---------TABELA SERVICO---------------
+Create table Servico (
+    id_servico INTEGER PRIMARY KEY AUTOINCREMENT,
+    nome_servico TEXT(100) UNIQUE NOT NULL,
+    descricao TEXT(200),
+    preco DECIMAL NOT NULL,
+    duracao_minutos INT
+);
 
-select * from Profissional;
-
-update Profissional
-    set nome = 'Marta'
-    where id_profissional = 3;
-
-select * from Profissional;
-
-select * from Profissional
-order BY nome;
+--------TABELA AGENDAMENTO--------------
+Create table Agendamento (
+    id_agendamento INTEGER PRIMARY KEY AUTOINCREMENT,
+    data_hora_inicio DATETIME,
+    data_hora_fim DATETIME,
+    status TEXT(15) NOT NULL,
+    valor_final DECIMAL,
+    ----FOREIGNS KAYS---------
+    id_cliente INTEGER NOT NULL,
+    id_profissional INTEGER NOT NULL,
+    id_servico INTEGER NOT NULL,
+    
+    FOREIGN KEY (id_cliente) REFERENCES Cliente(id_cliente),
+    FOREIGN KEY (id_profissional) REFERENCES Profissional(id_profissional),
+    FOREIGN KEY (id_servico) REFERENCES Servico(id_servico)
+);
